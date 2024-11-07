6b46a03af92593f9a51c5703bfe3fbb50fcd9d53
CPenalba <carolinapenalbacorpa@gmail.com>
2024-11-07 09:31:07 +0100

    Tabla multiplicar terminada

--- src/components/tablamultiplicar/tablamultiplicar.component.ts
+++ src/components/tablamultiplicar/tablamultiplicar.component.ts
@@ -7,17 +7,18 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 export class TablaMultiplicarComponent {
   @ViewChild('cajanumero') cajaNumeroRef!: ElementRef;
   public numeros: Array<number>;
-  public numero!: number;
+  public numero: number;
 
   constructor() {
     this.numeros = new Array<number>();
+    this.numero = 0;
   }
 
   generarTablaMultiplicar(): void {
     this.numero = parseInt(this.cajaNumeroRef.nativeElement.value);
-    var aux = new Array<number>();
-    for (let i = 1; i <= 10; i++) {
-      let resultado = this.numero * i;
+    let aux = new Array<number>();
+    for (var i = 1; i <= 10; i++) {
+      var resultado = this.numero * i;
       aux.push(resultado);
     }
     this.numeros = aux;
